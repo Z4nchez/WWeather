7825f5057e88d76edbea8900c665f7825c55892f
Zanchez <szwallie26@gmail.com>
2024-02-12 10:19:29 -0300

    Ajuste de color

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
 import Header from "../src/Components/Header/Header"
 import Panel from "../src/Components/Panel/Panel"
 import Footer from "../src/Components/Footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='Background0'>
+    <div className={styles.Background0}>
       <Header></Header>
       <Panel></Panel>
       <Footer></Footer>
